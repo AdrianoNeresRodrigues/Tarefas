b567805e67501c65f84c246e9892d15f1d4e36a1
AdianoNeres <118769051+AdianoNeres@users.noreply.github.com>
2022-12-01 15:00:01 -0300

    Ajustes

--- Tarefas/Models/Lista.cs
+++ Tarefas/Models/Lista.cs
@@ -7,10 +7,11 @@ namespace Tarefas.Models
         [Key]
         public int ListaId { get; set; }
 
-        [Required(ErrorMessage = "Necessário digitar um nome para a lista")]
-        [MinLength(5)]
-        [MaxLength(20)]
+        [Required(ErrorMessage = "Necessário digitar um nome para a lista"), MinLength(5), MaxLength(20)]
         public string? ListaNome { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
         public DateTime? ListaDataLembrete { get; set; }
         public string? ListaLembrete { get; set; }
         public Lista? ListaPai { get; set; }    
--- Tarefas/Models/Tarefa.cs
+++ Tarefas/Models/Tarefa.cs
@@ -8,16 +8,23 @@ namespace Tarefas.Models
         [Key]
         public int TarefaId { get; set; }
 
-        [Required(ErrorMessage = "Necessário digitar uma descrição para a tarefa")]
-        [MinLength(5)]
+        [Required(ErrorMessage = "Necessário digitar uma descrição para a tarefa"), MinLength(5)]
         public string? TarefaDescricao { get; set; }
         [Required(ErrorMessage = "Necessário digitar uma descrição para a tarefa")]
 
         public int TarefaPrioridade { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)] 
         public DateTime? TarefaDataCriacao { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)] 
         public DateTime? TarefaDataLembrete { get; set; }
         public string? TarefaLembrete { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)] 
         public DateTime? TarefaDataVencimento { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)] 
         public DateTime? TarefaDataConclusao { get; set; }
 
         public Lista? ListaPai { get; set; }
